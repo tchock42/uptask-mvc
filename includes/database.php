7aaa7b98cc3315a9075ae2ee8000231c65677e16
Jacob Gomez Carrillo <husky-coldjacob@hotmail.com>
2024-03-22 14:14:55 -0600

    Modificación de database.php

--- includes/database.php
+++ includes/database.php
@@ -1,10 +1,10 @@
 <?php
 
 $db = mysqli_connect(
-    $_ENV['BD_HOST'], 
-    $_ENV['BD_USER'], 
-    $_ENV['BD_PASS'], 
-    $_ENV['BD_NAME']
+    $_ENV['DB_HOST'], 
+    $_ENV['DB_USER'], 
+    $_ENV['DB_PASS'], 
+    $_ENV['DB_NAME']
 );
 $db->set_charset('utf8');
 
